f44a149e897a94f3da783119a2ef8b4445e35d28
Eduardo Costa <edujcosta@hotmail.com>
2020-01-17 16:51:40 -0300

    Criado o projeto Portifolio e todos os itens para funcionamento

--- semana1/sistema-loja/js/filtro.js
+++ semana1/sistema-loja/js/filtro.js
@@ -7,14 +7,14 @@ function funcao_pesquisa() {
     table = document.getElementById("pedidos");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
-      }       
     }
-  }
+}
