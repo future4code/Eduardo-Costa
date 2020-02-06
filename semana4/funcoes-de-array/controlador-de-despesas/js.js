72c4ed6f5c85dac19dc4de9cd07fcd2125a0e106
EduCosta85 <59846713+EduCosta85@users.noreply.github.com>
2020-02-06 13:04:08 -0300

    Passo 1

---
+++ semana4/funcoes-de-array/controlador-de-despesas/js.js
@@ -0,0 +1,8 @@
+function limpaCadastro() {
+    let campo = document.getElementById("valor")
+    campo.value = ""
+    campo = document.getElementById("descricao")
+    campo.value = ""
+    campo = document.getElementById("tipo")
+    campo.value = ""
+}
