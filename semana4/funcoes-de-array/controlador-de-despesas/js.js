66b099e9fc4af0c0ccc8090488c596af3ae010dc
EduCosta85 <59846713+EduCosta85@users.noreply.github.com>
2020-02-06 13:11:42 -0300

    Passo 2

--- semana4/funcoes-de-array/controlador-de-despesas/js.js
+++ semana4/funcoes-de-array/controlador-de-despesas/js.js
@@ -6,3 +6,16 @@ function limpaCadastro() {
     campo = document.getElementById("tipo")
     campo.value = ""
 }
+class Despesa {
+    constructor(valor, descricao, tipo) {
+        this.valor = valor
+        this.descricao = descricao
+        this.tipo = tipo
+    }
+}
+let despesas = []
+
+function cadastraDespesa(valor, descricao, tipo) {
+    let nDesp = new Despesa(valor, descricao, tipo)
+    despesas.push(nDesp)
+}
